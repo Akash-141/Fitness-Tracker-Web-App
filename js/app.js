68ee68a82eb8b3bf798a583edc7dc2d90c4e10fe
Akash Paul <akash17.paul@gmail.com>
2026-05-25 22:04:05 +0600

    Update print statement from 'Hello' to 'Goodbye'

--- js/app.js
+++ js/app.js
@@ -155,7 +155,6 @@ elements.toggleAuthMode.addEventListener('click', () => {
     }
 });
 
-
 elements.authForm.addEventListener('submit', async(e) => {
     e.preventDefault();
     const email = document.getElementById('authEmail').value.trim();
